224f4efd6d61626df220815e5f4e0cd817d6ad6e
IdR3n <bidrisarikaya@gmail.com>
2023-05-05 17:04:19 +0200

    SRUD,Search et Delete

--- www/controllers/ingredientController.php
+++ www/controllers/ingredientController.php
@@ -1,14 +1,23 @@
 <?php
 
-require_once("../models/Beer.php");
-require_once("./baseController.php");
+require_once("../models/Ingredient.php");
+require_once("../controllers/baseController.php");
 
 function read(int $id) {
 
 }
+//cette function est une partie de SCRUD, Search pour chercher un ingredient dans la table ingredient.
+function search():array
+{
 
-function search() {
+$ingredient = new Ingredient();
+$ingredients = $ingredient->search();
 
+$serializedIngredients = [];
+foreach ($ingredients as $ingredient) {
+    $serializedIngredients[] = serializeIngredient($ingredient);
+}
+return $serializedIngredients;
 }
 function create(array $body) {
 
@@ -17,8 +26,22 @@ function create(array $body) {
 function update(int $id, array $body) {
 
 }
-
+// cette function est une partie de SCRUD, Delete pour delete un ingredient dans la table ingredient.
 function delete(int $id) {
-
+    $ingredient = new Ingredient();
+    $message = $ingredient->delete($id);
+    return ["message" => $message];
 }
-
+//avec cette function on a change JSON pour objet
+function serializeIngredient(Ingredient $ingredient): array
+{
+    return [
+        'id'=>$ingredient->getId(),
+        "type"=>$ingredient->getType(),
+        "name"=>$ingredient->getName(),
+        "amount_value"=>$ingredient->getAmountValue(),
+        "amount_unit"=>$ingredient->getAmountUnit(),
+        "amount_add"=>$ingredient->getAmountAdd(),
+        "amount_attribute"=>$ingredient->getAmountAttribute()
+    ];
+}
--- www/models/Ingredient.php
+++ www/models/Ingredient.php
@@ -1,16 +1,114 @@
 <?php
 
-require_once("./Database.php");
+require_once("../models/Database.php");
 
 class Ingredient extends Database
 {
+    private ?int $id;
+    private $type;
+    private $name;
+    private $amountValue;
+    private $amountUnit;
+    private $amountAdd;
+    private $amountAttribute;
+
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function setId(?int $id) {
+        $this->id = $id;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getAmountValue()
+    {
+        return $this->amountValue;
+    }
+
+    public function setAmountValue($amountValue)
+    {
+        $this->amountValue = $amountValue;
+    }
+
+    public function getAmountUnit()
+    {
+        return $this->amountUnit;
+    }
+
+    public function setAmountUnit($amountUnit)
+    {
+        $this->amountUnit = $amountUnit;
+    }
+
+    public function getAmountAdd()
+    {
+        return $this->amountAdd;
+    }
+
+    public function setAmountAdd($amountAdd)
+    {
+        $this->amountAdd = $amountAdd;
+    }
+
+    public function getAmountAttribute()
+    {
+        return $this->amountAttribute;
+    }
+
+    public function setAmountAttribute($amounAttribute)
+    {
+        $this->amountAttribute = $amounAttribute;
+    }
     /**
      * Method to get all ingredients from db
      *
      * @return array
      */
     public function search(): array {
-        return [];
+        try {
+            $stmt = $this->pdo->prepare('SELECT * FROM ingredients');
+            $stmt->execute();
+            $ingredients = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+            $ingredientsObj = [];
+            foreach ($ingredients as $ingredient) {
+
+                $tempIngredient = new Ingredient();
+                $tempIngredient->setId($ingredient->id);
+                $tempIngredient->setType($ingredient->type);
+                $tempIngredient->setName($ingredient->name);
+                $tempIngredient->setAmountValue($ingredient->amount_value);
+                $tempIngredient->setAmountUnit($ingredient->amount_unit);
+                $tempIngredient->setAmountAdd($ingredient->amount_add);
+                $tempIngredient->setAmountAttribute($ingredient->amount_attribute);
+                $ingredientsObj[] = $tempIngredient;
+
+            }
+            return $ingredientsObj;
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 
     /**
@@ -50,7 +148,15 @@ class Ingredient extends Database
      * @return string
      */
     public function delete(int $id): string {
-
+        try {
+            $stmt = $this->pdo->prepare("DELETE FROM ingredients WHERE id = :id");
+            $stmt->execute([
+                "id" => $id
+            ]);
+            return "L'ingredient d'id $id a été supprimée";
+        } catch(Exception $e) {
+            throw $e;
+        }
     }
 
 }
