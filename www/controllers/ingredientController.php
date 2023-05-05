02ea51eab896e832caef5fdc1a7b3edca125f8b7
Daniel Sajdak <danielsajdak65@gmail.com>
2023-05-05 12:31:40 +0200

    get

--- www/controllers/ingredientController.php
+++ www/controllers/ingredientController.php
@@ -1,10 +1,15 @@
 <?php
 
-require_once("../models/Beer.php");
+require_once("../models/Ingredient.php");
 require_once("./baseController.php");
 
 function read(int $id) {
-
+    $ingr = new Ingredient();
+    $ingredient = $ingr->read($id);
+    
+  
+    return serializeIngredient($ingredient);
+   
 }
 
 function search() {
@@ -22,3 +27,14 @@ function delete(int $id) {
 
 }
 
+function serializeIngredient(Ingredient $ingr): array {
+    return [
+        'id' => $ingr->getId(),
+        'name' => $ingr->getName(),
+        "type" => $ingr->getType(),
+        "amountValue" => $ingr->getAmountValue(),
+        "amountUnit" => $ingr->getAmountUnit(),
+        "amountAdd" => $ingr->getAmountAdd(),
+        "amountAttribute" => $ingr->getAmountAttribute()
+    ];
+}
--- www/models/Ingredient.php
+++ www/models/Ingredient.php
@@ -1,9 +1,88 @@
 <?php
 
-require_once("./Database.php");
+require_once("../models/Database.php");
 
 class Ingredient extends Database
 {
+
+    private ?int $id;
+    private $type;
+    private $name;
+    private $amountValue;
+    private $amountUnit;
+    private $amountAdd;
+    private $amountAttribute;
+
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function setId(?int $id) {
+        $this->id = $id;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getAmountValue()
+    {
+        return $this->amountValue;
+    }
+
+    public function setAmountValue($amountValue)
+    {
+        $this->amountValue = $amountValue;
+    }
+
+    public function getAmountUnit()
+    {
+        return $this->amountUnit;
+    }
+
+    public function setAmountUnit($amountUnit)
+    {
+        $this->amountUnit = $amountUnit;
+    }
+
+    public function getAmountAdd()
+    {
+        return $this->amountAdd;
+    }
+
+    public function setAmountAdd($amountAdd)
+    {
+        $this->amountAdd = $amountAdd;
+    }
+
+    public function getAmountAttribute()
+    {
+        return $this->amountAttribute;
+    }
+
+    public function setAmountAttribute($amounAttribute)
+    {
+        $this->amountAttribute = $amounAttribute;
+    }
+
+
     /**
      * Method to get all ingredients from db
      *
@@ -30,7 +109,23 @@ class Ingredient extends Database
      * @return Ingredient
      */
     public function read(int $id): Ingredient {
+        $stmt = $this->pdo->prepare("SELECT * FROM ingredients WHERE :id");
+        $stmt->execute(['id' => $id]);
+        $ingredient = $stmt->fetch(PDO::FETCH_OBJ);
+        var_dump($ingredient);
 
+        $ingredientObj = new Ingredient();
+        $ingredientObj->setId($ingredient->id);
+        $ingredientObj->setName($ingredient->name);
+        $ingredientObj->setType($ingredient->type);
+        $ingredientObj->setAmountValue($ingredient->amount_value);
+        $ingredientObj->setAmountUnit($ingredient->amount_unit);
+        $ingredientObj->setAmountAdd($ingredient->amount_add);
+        $ingredientObj->setAmountAttribute($ingredient->amount_attribute);
+      
+        var_dump($ingredientObj);
+        
+        return $ingredientObj;
     }
 
     /**
