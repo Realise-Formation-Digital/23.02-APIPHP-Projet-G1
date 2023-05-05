fea6b8333c06934a6e09abe0bdc82341403a8981
Félicien Corbat <felicien@champis.net>
2023-05-05 09:45:49 +0200

    Add search beers

--- www/controllers/beerController.php
+++ www/controllers/beerController.php
@@ -7,8 +7,18 @@ function read(int $id) {
 
 }
 
-function search() {
+/**
+ * @throws Exception
+ */
+function search(): array {
+    $beer = new Beer();
+    $beers = $beer->search();
 
+    $serializedBeers = [];
+    foreach($beers as $beer) {
+        $serializedBeers[] = serializeBeer($beer);
+    }
+    return $serializedBeers;
 }
 function create(array $body) {
 
@@ -20,5 +30,23 @@ function update(int $id, array $body) {
 
 function delete(int $id) {
 
+}
+
+function serializeBeer(Beer $beer): array {
+    return [
+        'id' => $beer->getId(),
+        'name' => $beer->getName(),
+        "tagline" => $beer->getTagline(),
+        "first_brewed" => $beer->getFirstBrewed(),
+        "description" => $beer->getDescription(),
+        "image_url" => $beer->getImageUrl(),
+        "food_pairing" => [
+            $beer->getFoodPairing1(),
+            $beer->getFoodPairing2(),
+            $beer->getFoodPairing3()
+        ],
+        "brewers_tips" => $beer->getBrewersTips(),
+        "contribued_by" => $beer->getContribuedBy()
+    ];
 }
 
--- www/models/Beer.php
+++ www/models/Beer.php
@@ -1,32 +1,213 @@
 <?php
 
-require_once("./Database.php");
+require_once("../models/Database.php");
 
 class Beer extends Database
 {
 
-    private int $id;
+    private ?int $id;
+
+    private ?string $name;
+
+    private string $tagline;
+
+    private string $firstBrewed;
+
+    private string $description;
+
+    private string $imageUrl;
+
+    private string $brewersTips;
+
+    private string $contribuedBy;
+
+    private ?string $foodPairing1;
+
+    private ?string $foodPairing2;
+
+    private ?string $foodPairing3;
 
     /**
-     * @return int
+     * @return int|null
      */
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
     /**
-     * @param int $id
+     * @param int|null $id
      */
-    public function setId(int $id): void
+    public function setId(?int $id): void
     {
         $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTagline(): string
+    {
+        return $this->tagline;
+    }
+
+    /**
+     * @param string $tagline
+     */
+    public function setTagline(string $tagline): void
+    {
+        $this->tagline = $tagline;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstBrewed(): string
+    {
+        return $this->firstBrewed;
+    }
+
+    /**
+     * @param string $firstBrewed
+     */
+    public function setFirstBrewed(string $firstBrewed): void
+    {
+        $this->firstBrewed = $firstBrewed;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageUrl(): string
+    {
+        return $this->imageUrl;
+    }
+
+    /**
+     * @param string $imageUrl
+     */
+    public function setImageUrl(string $imageUrl): void
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBrewersTips(): string
+    {
+        return $this->brewersTips;
+    }
+
+    /**
+     * @param string $brewersTips
+     */
+    public function setBrewersTips(string $brewersTips): void
+    {
+        $this->brewersTips = $brewersTips;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContribuedBy(): string
+    {
+        return $this->contribuedBy;
+    }
+
+    /**
+     * @param string $contribuedBy
+     */
+    public function setContribuedBy(string $contribuedBy): void
+    {
+        $this->contribuedBy = $contribuedBy;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFoodPairing1(): ?string
+    {
+        return $this->foodPairing1;
+    }
+
+    /**
+     * @param string|null $foodPairing1
+     */
+    public function setFoodPairing1(?string $foodPairing1): void
+    {
+        $this->foodPairing1 = $foodPairing1;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFoodPairing2(): ?string
+    {
+        return $this->foodPairing2;
+    }
+
+    /**
+     * @param string|null $foodPairing2
+     */
+    public function setFoodPairing2(?string $foodPairing2): void
+    {
+        $this->foodPairing2 = $foodPairing2;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFoodPairing3(): ?string
+    {
+        return $this->foodPairing3;
+    }
+
+    /**
+     * @param string|null $foodPairing3
+     */
+    public function setFoodPairing3(?string $foodPairing3): void
+    {
+        $this->foodPairing3 = $foodPairing3;
     }
 
     /**
      * Method to get all beers from db
      *
      * @return array
+     * @throws Exception
      */
     public function search(): array {
         try {
@@ -37,16 +218,24 @@ class Beer extends Database
             $beersObj = [];
             foreach($beers as $beer) {
                 $tempBeer = new Beer();
-                $tempBeer->setId($beers->id);
+                $tempBeer->setId($beer->id);
+                $tempBeer->setName($beer->name);
+                $tempBeer->setTagline($beer->tagline);
+                $tempBeer->setFirstBrewed($beer->first_brewed);
+                $tempBeer->setDescription($beer->description);
+                $tempBeer->setImageUrl($beer->image_url);
+                $tempBeer->setBrewersTips($beer->brewers_tips);
+                $tempBeer->setContribuedBy($beer->contribued_by);
+                $tempBeer->setFoodPairing1($beer->food_pairing1);
+                $tempBeer->setFoodPairing2($beer->food_pairing2);
+                $tempBeer->setFoodPairing3($beer->food_pairing3);
                 $beersObj[] = $tempBeer;
             }
-
             return $beersObj;
 
         } catch(Exception $e) {
             throw $e;
         }
-        return [];
     }
 
     /**
@@ -88,5 +277,7 @@ class Beer extends Database
     public function delete(int $id): string {
 
     }
+
+
 
 }
